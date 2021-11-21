fbec2fc2887506568aa83b061e18726ae8f48324
johanreji <johanpenatu@gmail.com>
2021-11-21 16:44:53 +0530

    add category form - bug fix

--- src/Components/AddCategory.js
+++ src/Components/AddCategory.js
@@ -5,27 +5,31 @@ const AddCategoryForm = ({ handleCategoryAddition, categories }) => {
 
     const [name, setName] = useState('');
     const [url, setUrl] = useState("");
-    const [isNameError, setNameError] = useState('');
-    const [isUrlError, setUrlError] = useState('');
+    const [isNameError, setNameError] = useState(false);
+    const [isUrlError, setUrlError] = useState(false);
     const handleFormSubmit = (event) => {
         event.preventDefault();
+        setNameError(false);
+        setUrlError(false);
+        let isError = false;
         if (name === "" || categories.some(category => category.name === name)) {
             setNameError(true);
-            return;
+            isError = true;
         }
         if (url === "" || categories.some(category => category.url === url)) {
             setUrlError(true);
-            return;
+            isError = true;
         }
-        handleCategoryAddition(name, url)
+        if (!isError)
+            handleCategoryAddition(name, url)
     }
     return <div className="form-container">
         <div className='form-header'>Add Category</div>
         <form onSubmit={handleFormSubmit}>
             <input className='form-input' placeholder="Category Name" onChange={(e) => setName(e.target.value)} />
-            {isNameError && <span className="form-error">Enter an unique valid name</span>}
+            {isNameError && <div className="form-error">Enter an unique valid name</div>}
             <input className='form-input' placeholder="API URL" value={url} onChange={(e) => setUrl(e.target.value)} />
-            {isUrlError && <span className="form-error">Enter an valid URL</span>}
+            {isUrlError && <div className="form-error">Enter an unique valid URL</div>}
             <input className='form-submit' type="submit" value="Add" />
         </form>
     </div>
